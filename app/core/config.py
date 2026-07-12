a4c7b75849298d05f3a21142f723c8728bbc4bc3
Sabridge22 <Alegzandr2004@gmail.com>
2026-07-12 22:59:52 +0300

    настроил конфиг

---
+++ app/core/config.py
@@ -0,0 +1,21 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    DATABASE_URL: str
+    SECRET_KEY: str
+    JWT_ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+
+    DEBUG: bool = False
+    APP_NAME: str = 'Fade Chat'
+    APP_VERSION: str = "1.0.0"
+    ALLOWED_ORIGINS: list[str] = ["http://localhost:3000"]
+    DB_POOL_SIZE: int = 5
+    DB_MAX_OVERFLOW: int = 10
+    DB_ECHO: bool = False
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+settings = Settings()
