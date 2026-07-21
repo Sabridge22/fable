dbbc2dcbe6e1280e5168e600a68c2b51991c55ce
Sabridge22 <Alegzandr2004@gmail.com>
2026-07-21 17:12:32 +0300

    в сервисе реализована функция регистрации пользователя

---
+++ app/services/user.py
@@ -0,0 +1,66 @@
+from sqlalchemy.orm import Session
+from app.repositories.user import UserRepository
+from app.schemas.user import UserCreateSchema, UserResponseSchema, UserUpdateSchema
+
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+class UserNotFound(Exception):
+    """Пользователь не найден"""
+    ...
+
+class UserAlreadyExists(Exception):
+    """Пользователь уже существует"""
+    ...
+
+
+class UserService:
+    def __init__(self, db: Session) -> None:
+        self.db = db
+        self.user_repository = UserRepository(db)
+
+
+    def hash_password(self, password: str) -> str:
+        return pwd_context.hash(password)
+
+
+    def verify_password(self, plain_password: str, hashed_password: str) -> bool:
+        return pwd_context.verify(plain_password, hashed_password)
+
+
+    def register_user(self, user_data: UserCreateSchema) -> UserResponseSchema:
+        if self.user_repository.get_by_username(user_data.username) is not None:
+            raise UserAlreadyExists("Username already taken")
+        if self.user_repository.get_by_email(user_data.email) is not None:
+            raise UserAlreadyExists("Email already taken")
+        
+        hashed_password = self.hash_password(user_data.password)
+        new_user = self.user_repository.create(username=user_data.username, email=user_data.email, hashed_password=hashed_password)
+        self.db.commit()
+        self.db.refresh(new_user) # для подгрузки полей, генерируемых в бд
+        return UserResponseSchema.model_validate(new_user)
+
+
+    def get_user_by_id(self):
+        ...
+
+
+    def get_user_by_username(self):
+        ...
+
+
+    def get_user_by_email(self):
+        ...
+
+
+    def update_user(self):
+        ...
+    
+
+    def delete_user(self):
+        ...
+
+
+    def get_all_users(self):
+        ...
