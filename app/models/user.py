4ebf4bbc731d51935de5feb666dc610c012c1947
Sabridge22 <Alegzandr2004@gmail.com>
2026-07-21 15:26:15 +0300

    репозиторий пользователя готов

--- app/models/user.py
+++ app/models/user.py
@@ -8,8 +8,6 @@ class UserORM(Base):
     email: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
     hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
     username: Mapped[str] = mapped_column(String(100), nullable=False)
-    first_name: Mapped[str | None] = mapped_column(String(100), nullable=True)
-    last_name: Mapped[str | None] = mapped_column(String(100), nullable=True)
     language_level: Mapped[str] = mapped_column(String(5), default="A1")
     message_count: Mapped[int] = mapped_column(Integer, default=0)
     mistakes_count: Mapped[int] = mapped_column(Integer, default=0)
---
+++ app/repositories/user.py
@@ -0,0 +1,44 @@
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from app.models import UserORM
+
+
+class UserRepository:
+    def __init__(self, db: Session) -> None:
+        self.db = db
+
+    def get_by_id(self, user_id: str) -> UserORM | None:
+        return self.db.get(UserORM, user_id)
+    
+    def get_by_email(self, email: str) -> UserORM | None:
+        stmt = select(UserORM).where(UserORM.email == email)
+        return self.db.execute(stmt).scalar_one_or_none()
+    
+    def get_by_username(self, username: str) -> UserORM | None:
+        stmt = select(UserORM).where(UserORM.username == username)
+        return self.db.execute(stmt).scalar_one_or_none()
+    
+    def create(self, username: str, email: str, hashed_password: str) -> UserORM:
+        new_user = UserORM(username=username, email=email, hashed_password=hashed_password)
+        self.db.add(new_user)
+        return new_user
+    
+    def update(self, user: UserORM, data: dict, ) -> UserORM:
+        for key, value in data.items():
+            if hasattr(user, key):
+                setattr(user, key, value)
+
+        return user
+    
+    def delete(self, user: UserORM) -> None:
+        self.db.delete(user)
+
+    def delete_by_id(self, user_id: str) -> None:
+        user = self.get_by_username(user_id)
+        if user:
+            self.db.delete(user)
+
+    def get_all(self, limit: int = 10, offset: int = 0) -> list[UserORM]:
+        stmt = select(UserORM).limit(limit=limit).offset(offset=offset)
+        return list(self.db.scalars(stmt).all())
