5837bf9cce3176716027cc4157fe1b10c653271b
Sabridge22 <Alegzandr2004@gmail.com>
2026-07-21 18:36:12 +0300

    добавлена ручка получения пользователя по id

--- app/api/routers/user.py
+++ app/api/routers/user.py
@@ -8,12 +8,21 @@ from app.services.user import UserNotFound, UserAlreadyExists, UserService
 from app.db.session import get_db
 from typing import Annotated
 
-router = APIRouter(prefix="/users")
+router = APIRouter(prefix="/users", tags=["Users"])
 
-@router.post('', response_model=UserResponseSchema, status_code=status.HTTP_201_CREATED)
-def create_user(user_data: UserCreateSchema, db: Session = Depends(get_db)):
+@router.post('/register', response_model=UserResponseSchema, status_code=status.HTTP_201_CREATED)
+def register_user(user_data: UserCreateSchema, db: Session = Depends(get_db)):
     service = UserService(db)
     try:
         return service.register_user(user_data=user_data)
     except UserAlreadyExists as e:
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=str(e))
+    
+
+@router.get('/{user_id}', response_model=UserResponseSchema, status_code=status.HTTP_200_OK)
+def get_user_by_id(user_id: str, db: Session = Depends(get_db)):
+    service = UserService(db)
+    try:
+        return service.get_user_by_id(user_id=user_id)
+    except UserNotFound as e:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
