54d0003f98612f34f43e40c8afc4921c1157b42b
Sabridge22 <Alegzandr2004@gmail.com>
2026-07-30 21:44:02 +0300

    создан security для создания и чтения jwt токена

---
+++ app/core/security.py
@@ -0,0 +1,15 @@
+from datetime import datetime, timedelta, timezone
+from jose import jwt
+from app.core.config import settings
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None) -> str:
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.JWT_ALGORITHM)
+
+def decode_access_token(token: str) -> dict:
+    return jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.JWT_ALGORITHM])
