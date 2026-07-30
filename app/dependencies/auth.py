7cab7099bf81b5743ab3a3b3ec0a0ea10c1169ef
Sabridge22 <Alegzandr2004@gmail.com>
2026-07-30 21:50:12 +0300

    создана зависимость get_current_user

---
+++ app/dependencies/auth.py
@@ -0,0 +1,30 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError
+from sqlalchemy.orm import Session
+from app.db.session import get_db
+from app.core.security import decode_access_token
+from app.services.user import UserService, UserNotFound
+from app.models.user import UserORM
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
+
+async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)) -> UserORM:
+    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
+    
+    try:
+        payload = decode_access_token(token)
+        user_id: str = payload.get("sub")
+        if user_id is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    
+    service = UserService(db)
+    try:
+        user = service.get_user_by_id(user_id)
+    except UserNotFound:
+        raise credentials_exception
+    
+    return user
