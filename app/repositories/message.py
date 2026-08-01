841c21660a2cf4ad17af38949454e50a7b6cf4ef
Sabridge22 <Alegzandr2004@gmail.com>
2026-08-01 22:03:03 +0300

    создан метод получения сообщения по id в репозитории

--- app/repositories/message.py
+++ app/repositories/message.py
@@ -15,4 +15,7 @@ class MessageRepository:
     def get_by_user_id(self, user_id: str, limit: int = 100, offset: int = 0) -> list[MessageORM]:
         stmt = select(MessageORM).where(MessageORM.user_id == user_id).limit(limit).offset(offset).order_by(MessageORM.created_at.desc())
         return list(self.db.scalars(stmt).all())
+
+    def get_by_id(self, message_id: str) -> MessageORM | None:
+        return self.db.get(MessageORM, message_id)
     
