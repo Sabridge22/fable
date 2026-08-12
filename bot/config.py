d86b0dbc794251f3c8d8ce9d8ba4bbd93b67c05a
Sabridge22 <Alegzandr2004@gmail.com>
2026-08-12 19:04:12 +0300

    начал писать бота тг, написал конфиг, добавил необходимые данные в .env

---
+++ bot/config.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class BotConfig:
+    TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+    API_BASE_URL = os.getenv("API_BASE_URL")
+
+    if not TELEGRAM_BOT_TOKEN:
+        raise ValueError("TELEGRAM_BOT_TOKEN not set in .env")
+
+    # эндпоинты
+    REGISTER_URL = f"{API_BASE_URL}/users/register"
+    LOGIN_URL = f"{API_BASE_URL}/auth/login"
+    CHAT_URL = f"{API_BASE_URL}/chat"
